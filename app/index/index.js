33ca2f3392764e9a38185797906ce974bd50cf2f
瞿龙俊 <154256698@qq.com>
2017-07-12 20:02:56 +0800

    引入相关模板文件

--- app/index/index.js
+++ app/index/index.js
@@ -6,8 +6,11 @@ import store from './store/store';
 import router from './router/router'
 import 'mod/jquery'
 import 'bootstrap'
+import 'mediaelement/'
+import 'isotope-layout/dist/isotope.pkgd'
 
-
+import './script/plugins'
+import './script/functions'
 //===========导入Vue组件==============
 import App from './app.vue'
 import Favlist from './components/Favlist.vue'
@@ -16,7 +19,14 @@ import Favlist from './components/Favlist.vue'
 //==========导入CSS文件===============
 import './style/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
+// import './style/mediaelementplayer.css'
+import 'mediaelement/build/mediaelementplayer.min.css'
+import './style/magnific-popup.css'
+import './style/animate.min.css'
+import './style/icon-fonts.css'
 
+
+import './less/style.less'
 
 Vue.config.debug = true;//开启错误提示
 Vue.use(VueResource);
@@ -25,8 +35,8 @@ Vue.http.options.emulateJSON = true;
 //注册全局组件
 Vue.component('my-component', Favlist);
 
-new Vue({
-    render: h => h(App),
-    store,
-    router
-}).$mount('#app');
+// new Vue({
+//     render: h => h(App),
+//     store,
+//     router
+// }).$mount('#app');
---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,31 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require('path');
+var webpack = require('webpack');
+// 引入基本配置
+var config = require('./webpack.config');
+
+
+config.output.publicPath = '/';
+
+config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'html-withimg-loader!' + path.resolve(__dirname, '../app/index/index.html'),
+        inject: true
+    }),
+    new ExtractTextPlugin("styles.css")
+];
+
+// 动态向入口配置中注入 webpack-hot-middleware/client
+// var devClient = 'webpack-hot-middleware/client';
+var devClient = './build/dev-client';
+Object.keys(config.entry).forEach(function (name, i) {
+    var extras = [devClient]
+    config.entry[name] = extras.concat(config.entry[name])
+})
+
+module.exports = config;
