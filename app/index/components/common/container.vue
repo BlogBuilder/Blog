c4cc0aeedba8044fbd1f4710662ff312aa43ccf8
瞿龙俊 <154256698@qq.com>
2017-11-29 19:09:28 +0800

    调整相关

--- app/index/components/common/container.vue
+++ app/index/components/common/container.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="content_section">
-        <div class="row_spacer hidden-xs" style="height: 100vh;border: 1px dotted #e9e9e9 ">
-            <div class="text-center" style="position: relative;top: 50%;transform: translateY(-50%);">
-                <img src="../../image/logo-dangGui.png" width="40%" height="40%"
-                     style="max-width: 100%;max-height: 100%;"/>
-            </div>
-        </div>
+        <!--<div class="row_spacer hidden-xs" style="height: 100vh;border: 1px dotted #e9e9e9 ">-->
+            <!--<div class="text-center" style="position: relative;top: 50%;transform: translateY(-50%);">-->
+                <!--<img src="../../image/logo-dangGui.png" width="40%" height="40%"-->
+                     <!--style="max-width: 100%;max-height: 100%;"/>-->
+            <!--</div>-->
+        <!--</div>-->
         <div class="content row_spacer">
             <div class="content_spacer clearfix">
                 <router-view></router-view>
